c9a177f52fc39cd0af888fa6248757db1aae07c6
PiAlYu <157111194+PiAlYu@users.noreply.github.com>
2024-04-16 22:35:49 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -8,7 +8,15 @@ ops = {
     '/' : operator.truediv,
     '%' : operator.mod,
     '^' : operator.pow,
+    '<' : operator.lt,
+    '<=' : operator.le,
+    '==' : operator.eq,
+    '!=' : operator.ne,
+    '>=' : operator.ge,
+    '>' : operator.gt,
 }
+
+h = '><'
 
 def move(array: tuple, moves: str):
     moves = moves.split()
@@ -19,25 +27,47 @@ def move(array: tuple, moves: str):
     return fin_array
 
 @lru_cache(None)
-def game_sum(array: tuple, fin_value: int, fin_pos: str, moves: str, player = 1, step = 1):
-    if sum(array) >= fin_value and str(step) in fin_pos:
+def game_all_sum(array: tuple, fin_value: str, fin_pos: str, moves: str, player = 1, step = 1):
+    h1 = [i for i in h if i not in fin_value[0:2]][0]
+    if ops[fin_value[0:2]](sum(array), int(fin_value[2:])) and str(step) in fin_pos:
         return True
-    if (sum(array) < fin_value and step == fin_pos[-1]) or (sum(array) >= fin_value and step != fin_pos[-1]):
+    if (ops[h1](sum(array), int(fin_value[2:])) and str(step) == fin_pos[-1]) or (ops[fin_value[0:2]](sum(array), int(fin_value[2:])) and str(step) != fin_pos[-1]):
         return False
     if step % 2 == player % 2:
-        return any(game_sum(i, fin_value, fin_pos, moves, player, step + 1) for i in move(array, moves))
-    return all(game_sum(i, fin_value, fin_pos, moves, player, step + 1) for i in move(array, moves))
+        return any(game_all_sum(i, fin_value, fin_pos, moves, player, step + 1) for i in move(array, moves))
+    return all(game_all_sum(i, fin_value, fin_pos, moves, player, step + 1) for i in move(array, moves))
+
+@lru_cache(None)
+def game_any_sum(array: tuple, fin_value: int, fin_pos: str, moves: str, player = 1, step = 1):
+    if sum(array) >= fin_value and str(step) in fin_pos:
+        return True
+    if (sum(array) < fin_value and str(step) == fin_pos[-1]) or (sum(array) >= fin_value and str(step) != fin_pos[-1]):
+        return False
+    if step % 2 == player % 2:
+        return any(game_any_sum(i, fin_value, fin_pos, moves, player, step + 1) for i in move(array, moves))
+    return all(game_any_sum(i, fin_value, fin_pos, moves, player, step + 1) for i in move(array, moves))
 
 @lru_cache(None)
 def game_any(array: tuple, fin_value: int, fin_pos: str, moves: str, player = 1, step = 1):
     if any(i >= fin_value for i in array) and str(step) in fin_pos:
         return True
-    if (all(i < fin_value for i in array) and step == fin_pos[-1]) or (any(i >= fin_value for i in array) and step != fin_pos[-1]):
+    if (all(i < fin_value for i in array) and str(step) == fin_pos[-1]) or (any(i >= fin_value for i in array) and str(step) != fin_pos[-1]):
         return False
     if step % 2 == player % 2:
         return any(game_any(i, fin_value, fin_pos, moves, player, step + 1) for i in move(array, moves))
-    return all(game_any(i, fin_value, fin_pos, moves, player, step + 1) for i in move(array, moves))
+    return any(game_any(i, fin_value, fin_pos, moves, player, step + 1) for i in move(array, moves))
 
-for i in range(2, 43):
-    if game_any(tuple([3, i]), 49, '2', '+1 ^3', 1):
+@lru_cache(None)
+def game_all(array: tuple, fin_value: str, fin_pos: str, moves: str, player = 1, step = 1):
+    h1 = [i for i in h if i not in fin_value[0:2]][0]
+    if any(ops[fin_value[0:2]](i, int(fin_value[2:])) for i in array) and str(step) in fin_pos:
+        return True
+    if (all(ops[h1](i, int(fin_value[2:])) for i in array) and str(step) == fin_pos[-1]) or (any(ops[fin_value[0:2]](i, int(fin_value[2:])) for i in array) and str(step) != fin_pos[-1]):
+        return False
+    if step % 2 == player % 2:
+        return any(game_all(i, fin_value, fin_pos, moves, player, step + 1) for i in move(array, moves))
+    return all(game_all(i, fin_value, fin_pos, moves, player, step + 1) for i in move(array, moves))
+
+for i in range(1, 37):
+    if game_all_sum(tuple([i]), 38, '4', '+1 +2 *2', 1):
         print(i)
